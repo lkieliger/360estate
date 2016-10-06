8f958ba0a3ed00880787b66218309310ec5fc536
KENTINB <krentin@hotmail.fr>
2016-10-06 22:37:14 +0200

    Filter Interface done, adjustment fixed between the views.

--- app/src/main/java/ch/epfl/sweng/project/ListActivity.java
+++ app/src/main/java/ch/epfl/sweng/project/ListActivity.java
@@ -1,11 +1,19 @@
 package ch.epfl.sweng.project;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.parse.FindCallback;
@@ -20,12 +28,26 @@ import ch.epfl.sweng.project.list.ItemAdapter;
 
 public class ListActivity extends AppCompatActivity {
 
+    private static final int MIN_VALUE_PRICE = 100000;
+    private static final int MAX_VALUE_PRICE = 500000;
+    private static final int MIN_VALUE_SURFACE = 20;
+    private static final int MAX_VALUE_SURFACE = 400;
+
+
     ListView listView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        Button popupButton = (Button)findViewById(R.id.filterButtonPopUp);
+        popupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                initPopUpFilter();
+            }
+        });
 
         // Get ListView object from xml
         listView = (ListView) findViewById(R.id.houseList);
@@ -100,4 +122,38 @@ public class ListActivity extends AppCompatActivity {
         });
     }
 
+    void initPopUpFilter(){
+
+        final AlertDialog.Builder helpBuilder = new AlertDialog.Builder(this);
+        helpBuilder.setTitle("");
+
+        LayoutInflater inflater = getLayoutInflater();
+
+        final ViewGroup nullParent = null;
+        final View popupLayout = inflater.inflate(R.layout.popup_filter,nullParent);
+        helpBuilder.setView(popupLayout);
+
+        final AlertDialog helpDialog = helpBuilder.create();
+        helpDialog.show();
+
+        AutoCompleteTextView textView  = (AutoCompleteTextView) popupLayout.findViewById(R.id.autoCompleteTextView);
+        final String[] cities = new String[]{
+                "Geneve","Renens","Lausanne"
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_dropdown_item_1line, cities);
+        textView.setAdapter(adapter);
+
+        showSeekBar(popupLayout,R.id.seekBarPrice,R.id.ShowPrice,MIN_VALUE_PRICE,MAX_VALUE_PRICE,"Chf");
+        showSeekBar(popupLayout,R.id.seekBarSurface,R.id.ShowSurface,MIN_VALUE_SURFACE,MAX_VALUE_SURFACE,"m^2");
+
+    }
+
+    private void showSeekBar(View view,int seekBarId,int textId, int minValue, int maxValue,String units){
+        SeekBar seekBar = (SeekBar)view.findViewById(seekBarId);
+        TextView text = (TextView)view.findViewById(textId);
+        SeekBar.OnSeekBarChangeListener seekBarListenerPrice   = new MyOnSeekBarChangeListener(
+                text,minValue,maxValue,units);
+        seekBar.setOnSeekBarChangeListener(seekBarListenerPrice);
+    }
 }
---
+++ app/src/main/java/ch/epfl/sweng/project/MyOnSeekBarChangeListener.java
@@ -0,0 +1,40 @@
+package ch.epfl.sweng.project;
+
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+class MyOnSeekBarChangeListener implements SeekBar.OnSeekBarChangeListener {
+    private TextView textShow;
+    private final int minValue;
+    private final int maxValue;
+    private final String units;
+    private int progressChanged;
+
+    MyOnSeekBarChangeListener(TextView textShow, int minValue, int maxValue, String units) {
+        this.maxValue = maxValue;
+        this.textShow = textShow;
+        this.minValue = minValue;
+        this.units = units;
+        progressChanged = 0;
+    }
+
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        progressChanged = progress;
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        if(maxValue>minValue) {
+            int res = progressChanged * (maxValue-minValue) / seekBar.getMax() + minValue;
+            textShow.setText(String.format("%1$d "+units,res));
+        }
+    }
+}
+
