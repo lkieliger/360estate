69870cc7f8363981d7798c38d7ff77a035dcbbed
Quentin Bacuet <krentin@hotmail.fr>
2016-10-07 12:27:54 +0200

    Code cleanup.

--- app/src/main/java/ch/epfl/sweng/project/ListActivity.java
+++ app/src/main/java/ch/epfl/sweng/project/ListActivity.java
@@ -3,7 +3,6 @@ package ch.epfl.sweng.project;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
