11a9c6c20360adcf53fc60836de86f69dc137585
Leandro Kieliger <leandro.kieliger@epfl.ch>
2016-10-12 18:34:41 +0200

    Implementing the panorama activity

--- app/src/main/java/ch/epfl/sweng/project/engine3d/PanoramaActivity.java
+++ app/src/main/java/ch/epfl/sweng/project/engine3d/PanoramaActivity.java
@@ -1,13 +1,44 @@
 package ch.epfl.sweng.project.engine3d;
 
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.rajawali3d.renderer.Renderer;
+import org.rajawali3d.view.ISurface;
+import org.rajawali3d.view.SurfaceView;
+
+import ch.epfl.sweng.project.R;
 
 public class PanoramaActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState){
+    private static final String TAG = "PanoramaActivity";
 
+    private SurfaceView mSurface;
+    private Renderer mRenderer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSurface = new SurfaceView(this);
+        mSurface.setFrameRate(60.0);
+        mSurface.setRenderMode(ISurface.RENDERMODE_WHEN_DIRTY);
+
+        // Add mSurface to root view
+        addContentView(mSurface, new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT));
+
+        mRenderer = new PanoramaRenderer(this);
+        mSurface.setSurfaceRenderer(mRenderer);
+
+
+        //Create listener for handling user inputs
+        View.OnTouchListener listener = new PanoramaTouchListener();
+
+        mSurface.setOnTouchListener(listener);
     }
 }
