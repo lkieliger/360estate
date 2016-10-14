6e26715dc32f9805bfece4188c4465a4ea0698f7
Quentin Bacuet <krentin@hotmail.fr>
2016-10-14 13:01:35 +0200

    Fixies language issues with tests

--- app/src/androidTest/java/ch/epfl/sweng/project/FilterActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/project/FilterActivityTest.java
@@ -1,18 +1,21 @@
 package ch.epfl.sweng.project;
 
-import android.os.Build;
-import android.support.test.espresso.UiController;
+import android.support.test.espresso.PerformException;
 import android.support.test.espresso.ViewAction;
-import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.espresso.action.CoordinatesProvider;
+import android.support.test.espresso.action.GeneralSwipeAction;
+import android.support.test.espresso.action.Press;
+import android.support.test.espresso.action.Swipe;
+import android.support.test.espresso.util.HumanReadables;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.test.mock.MockContext;
 import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.SeekBar;
 
 import org.hamcrest.Description;
-import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
@@ -20,6 +23,7 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.actionWithAssertions;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
@@ -36,7 +40,7 @@ import static org.hamcrest.core.AllOf.allOf;
 @RunWith(AndroidJUnit4.class)
 public class FilterActivityTest {
 
-    private final String TAG = "FilterActivityTest: ";
+    private static final String TAG = "FilterActivityTest: ";
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
@@ -67,7 +71,7 @@ public class FilterActivityTest {
 
         for (int i = 0; i < counts[0]; i++) {
             onData(anything()).inAdapterView(withId(R.id.houseList)).atPosition(i).
-                    check(matches(hasDescendant(withText(containsString("3 rooms")))));
+                    check(matches(hasDescendant(withText(containsString("3 " + getString(R.string.rooms))))));
         }
     }
 
@@ -108,7 +112,7 @@ public class FilterActivityTest {
 
         onView(withId(R.id.filterButtonPopUp)).perform(click());
         onView(withId(R.id.spinner)).perform(click());
-        onData(allOf(is(instanceOf(String.class)), is("Building"))).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is(getString(R.string.building)))).perform(click());
         onView(withId(R.id.filterButton)).perform(click());
 
         final int[] counts = {0};
@@ -116,7 +120,7 @@ public class FilterActivityTest {
 
         for (int i = 0; i < counts[0]; i++) {
             onData(anything()).inAdapterView(withId(R.id.houseList)).atPosition(i).
-                    check(matches(hasDescendant(withText(containsString("Building")))));
+                    check(matches(hasDescendant(withText(containsString(getString(R.string.building))))));
         }
     }
 
@@ -133,9 +137,9 @@ public class FilterActivityTest {
         }
 
         onView(withId(R.id.filterButtonPopUp)).perform(click());
-        onView(withId(R.id.seekBarPrice)).perform(click());
-        onView(withId(R.id.seekBarPrice)).perform(setProgress(7));
+        onView(withId(R.id.seekBarPrice)).perform(scrubSeekBarAction(5));
         onView(withId(R.id.filterButton)).perform(click());
+
 
         final int[] counts = {0};
         onView(withId(R.id.houseList)).check(matches(new ViewTypeSafeMatcher(counts)));
@@ -158,9 +162,7 @@ public class FilterActivityTest {
         }
 
         onView(withId(R.id.filterButtonPopUp)).perform(click());
-        onView(withId(R.id.seekBarSurface)).perform(click());
-        onView(withId(R.id.seekBarSurface)).perform((setProgress(100)));
-
+        onView(withId(R.id.seekBarSurface)).perform(scrubSeekBarAction(50));
         onView(withId(R.id.filterButton)).perform(click());
 
         final int[] counts = {0};
@@ -193,30 +195,44 @@ public class FilterActivityTest {
         }
     }
 
-    public static ViewAction setProgress(final int progress) {
-        return new MyViewAction(progress);
+    public static ViewAction scrubSeekBarAction(int progress) {
+        return actionWithAssertions(new GeneralSwipeAction(
+                Swipe.SLOW,
+                new SeekBarThumbCoordinatesProvider(0),
+                new SeekBarThumbCoordinatesProvider(progress),
+                Press.PINPOINT));
     }
 
-    private static class MyViewAction implements ViewAction {
-        private final int progress;
+    private static class SeekBarThumbCoordinatesProvider implements CoordinatesProvider {
+        int mProgress;
 
-        MyViewAction(int progress) {
-            this.progress = progress;
+        public SeekBarThumbCoordinatesProvider(int progress) {
+            mProgress = progress;
+        }
+
+        private static float[] getVisibleLeftTop(View view) {
+            final int[] xy = new int[2];
+            view.getLocationOnScreen(xy);
+            return new float[]{ (float) xy[0], (float) xy[1] };
         }
 
         @Override
-        public void perform(UiController uiController, View view) {
-            ((SeekBar) view).setProgress(progress);
+        public float[] calculateCoordinates(View view) {
+            if (!(view instanceof SeekBar)) {
+                throw new PerformException.Builder()
+                        .withViewDescription(HumanReadables.describe(view))
+                        .withCause(new RuntimeException(String.format("SeekBar expected"))).build();
+            }
+            SeekBar seekBar = (SeekBar) view;
+            int width = seekBar.getWidth() - seekBar.getPaddingLeft() - seekBar.getPaddingRight();
+            double progress = mProgress == 0 ? seekBar.getProgress() : mProgress;
+            int xPosition = (int) (seekBar.getPaddingLeft() + width * progress / seekBar.getMax());
+            float[] xy = getVisibleLeftTop(seekBar);
+            return new float[]{ xy[0] + xPosition, xy[1] + 10 };
         }
+    }
 
-        @Override
-        public String getDescription() {
-            return "Set a progress";
-        }
-
-        @Override
-        public Matcher<View> getConstraints() {
-            return ViewMatchers.isAssignableFrom(SeekBar.class);
-        }
+    private String getString(int id){
+        return listActivityActivityTestRule.getActivity().getString(id);
     }
 }
