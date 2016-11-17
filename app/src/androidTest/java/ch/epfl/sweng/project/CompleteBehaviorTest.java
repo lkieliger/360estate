91377be345b401fdd11d47fd072abe6e4d43a6f9
Clément Nussbaumer <clement@astutus.org>
2016-11-17 10:38:37 +0100

    incorporating all android connected tests into a single test

--- app/src/androidTest/java/ch/epfl/sweng/project/CompleteBehaviorTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/project/CompleteBehaviorTest.java
@@ -6,6 +6,7 @@ import android.support.test.rule.ActivityTestRule;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
+import android.widget.ListView;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
@@ -16,8 +17,6 @@ import org.junit.Test;
 
 import java.security.SecureRandom;
 
-import ch.epfl.sweng.project.user.LoginActivity;
-
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.pressBack;
@@ -26,14 +25,20 @@ import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard
 import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static ch.epfl.sweng.project.util.TestUtilityFunctions.logUserOut;
 import static ch.epfl.sweng.project.util.TestUtilityFunctions.wait1s;
 import static ch.epfl.sweng.project.util.TestUtilityFunctions.wait250ms;
 import static ch.epfl.sweng.project.util.TestUtilityFunctions.wait500ms;
 import static ch.epfl.sweng.project.util.TestUtilityFunctions.waitNms;
 import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.AllOf.allOf;
 
 public class CompleteBehaviorTest {
 
@@ -48,6 +53,7 @@ public class CompleteBehaviorTest {
     @Rule
     public ActivityTestRule<SplashActivity> mActivityTestRule = new ActivityTestRule<>(SplashActivity.class);
 
+    @SuppressWarnings("AnonymousInnerClassWithTooManyMethods")
     private static Matcher<View> childAtPosition(
             final Matcher<View> parentMatcher, final int position) {
 
@@ -59,10 +65,10 @@ public class CompleteBehaviorTest {
             }
 
             @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
+            public boolean matchesSafely(View item) {
+                ViewParent parent = item.getParent();
                 return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
+                        && item.equals(((ViewGroup) parent).getChildAt(position));
             }
         };
     }
@@ -112,6 +118,56 @@ public class CompleteBehaviorTest {
 
         onView(withId(R.id.activity_list)).check(matches(isDisplayed()));
         wait1s(TAG);
+
+        // Incorporated the Filter tests
+
+        onView(withId(R.id.filterButtonPopUp)).perform(click());
+        onView(withId(R.id.MaxRooms)).perform(typeText("3"), closeSoftKeyboard());
+        onView(withId(R.id.MinRooms)).perform(typeText("3"), closeSoftKeyboard());
+        onView(withId(R.id.MaxSurface)).perform(typeText("2000000"), closeSoftKeyboard());
+        onView(withId(R.id.MinSurface)).perform(typeText("2000000"), closeSoftKeyboard());
+
+        onView(withId(R.id.MaxPrice)).perform(typeText("100"), closeSoftKeyboard());
+        onView(withId(R.id.MinPrice)).perform(typeText("100"), closeSoftKeyboard());
+
+        onView(withId(R.id.location)).perform(typeText("Renens"), closeSoftKeyboard());
+        onView(withId(R.id.spinner)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is(getString(R.string.building)))).perform(click());
+
+
+        onView(withId(R.id.filterButton)).perform(click());
+        wait250ms(TAG);
+
+        final int[] counts = {0};
+        onView(withId(R.id.houseList)).check(matches(new ViewTypeSafeMatcher(counts)));
+
+        for (int i = 0; i < counts[0]; i++) {
+            onData(anything()).inAdapterView(withId(R.id.houseList)).atPosition(i).
+                    check(matches(hasDescendant(withText(containsString(
+                            String.format(
+                                    getString(R.string.text_location_surface),
+                                    "Renens",
+                                    "2'000'000",
+                                    "3",
+                                    getString(R.string.rooms)
+                            ))))));
+            onData(anything()).inAdapterView(withId(R.id.houseList)).atPosition(i).
+                    check(matches(hasDescendant(withText(containsString(
+                            String.format(
+                                    getString(R.string.text_price_type),
+                                    "100",
+                                    getString(R.string.text_currency),
+                                    getString(R.string.building)
+                            ))))));
+        }
+
+        onView(withId(R.id.filterButtonPopUp)).perform(click());
+        wait250ms(TAG);
+        onView(withId(R.id.eraseButton)).perform(click());
+        onView(withId(R.id.filterButton)).perform(click());
+        wait250ms(TAG);
+
+
         onData(anything()).inAdapterView(withId(R.id.houseList)).atPosition(0).perform(click());
         onView(withId(R.id.addToFavorites)).perform(click());
         pressBack();
@@ -144,6 +200,30 @@ public class CompleteBehaviorTest {
 
         logUserOut();
 
+    }
+
+
+    private String getString(int id) {
+        return mActivityTestRule.getActivity().getString(id);
+    }
+
+    private static class ViewTypeSafeMatcher extends TypeSafeMatcher<View> {
+        private final int[] counts;
+
+        ViewTypeSafeMatcher(int[] extCounts) {
+            counts = extCounts;
+        }
+
+        @Override
+        public void describeTo(Description description) {
+        }
+
+        @Override
+        public boolean matchesSafely(View item) {
+            ListView listView = (ListView) item;
+            counts[0] = listView.getCount();
+            return true;
+        }
     }
 
 }
