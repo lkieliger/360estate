e64cda6a35837f1283c8e9c546dac68203ff14d5
Quentin Bacuet <krentin@hotmail.fr>
2016-11-20 12:59:02 +0100

    Added the possibility to had any length of text

--- app/src/main/java/ch/epfl/sweng/project/engine3d/StringAdapter.java
+++ app/src/main/java/ch/epfl/sweng/project/engine3d/StringAdapter.java
@@ -9,14 +9,11 @@ import android.graphics.Rect;
 import java.util.ArrayList;
 import java.util.List;
 
-
-/**
- * Created by Quentin on 19/11/2016.
- */
-
 public class StringAdapter {
 
+    private static final int EPSILON = 30;
     private final String text;
+
 
     public StringAdapter(String text) {
         this.text = text;
@@ -47,89 +44,65 @@ public class StringAdapter {
     }
 
 
-    public Bitmap textToBitmap(int textSize, int widthBitmap, int colorIndex) {
-
-        int epsilon = 30;
+    private List<String> textToList(int textSize, int widthBitmap) {
 
         Paint paint = new Paint();
         paint.setTextSize(textSize);
         Rect rect = new Rect();
-        paint.getTextBounds(text, 0, text.length(), rect);
-        final int heightTemp = rect.height();
-
-        paint.getTextBounds("a a", 0, 3, rect);
-        int temp = rect.width();
-        paint.getTextBounds("a", 0, 1, rect);
-        int spaceSize = temp - rect.width() + 1;
 
         String[] words = text.split(" ");
 
         //  int numberOfPixelPerLine = widthTemp/width;
 
-        List<StringBuilder> builderArrayList = new ArrayList<>();
-        builderArrayList.add(new StringBuilder());
+        List<StringBuilder> stringArrayList = new ArrayList<>();
+        stringArrayList.add(new StringBuilder());
 
-        int numberOfPixels = 0;
         int indexOfLine = 0;
         for (int i = 0; i < words.length; i++) {
-            paint.getTextBounds(words[i], 0, words[i].length(), rect);
-            final int widthWord = rect.width();
 
+            paint.getTextBounds(stringArrayList.get(indexOfLine).toString() + " " + words[i], 0, words[i].length()
+                    + stringArrayList.get(indexOfLine).length() + 1, rect);
+            final int widthLine = rect.width();
 
-            if (words[i].contains("\n")) {
-
-                String[] spitedWord = words[i].split("\n");
-                int widthWordSpited = 0;
-                if (spitedWord.length != 0 && !spitedWord[0].isEmpty()) {
-                    paint.getTextBounds(spitedWord[0], 0, spitedWord[0].length(), rect);
-                    widthWordSpited = rect.width();
-                }
-
-
-                if (numberOfPixels + widthWordSpited + spaceSize >= widthBitmap - 2 * epsilon) {
-                    indexOfLine++;
-                    StringBuilder stringBuilder = new StringBuilder();
-                    stringBuilder.append(words[i]);
-                    builderArrayList.add(stringBuilder);
-                    indexOfLine++;
-                    builderArrayList.add(new StringBuilder());
-                } else {
-                    builderArrayList.get(indexOfLine).append(" " + words[i]);
-                    indexOfLine++;
-                    builderArrayList.add(new StringBuilder());
-                }
-
-                for (int j = 1; j < spitedWord.length; j++) {
-                    StringBuilder stringBuilder = new StringBuilder();
-                    stringBuilder.append(words[i]);
-                    builderArrayList.add(stringBuilder);
-                }
-
-                numberOfPixels = 0;
+            if (words[i].equals("\n")) {
+                indexOfLine++;
+                stringArrayList.add(new StringBuilder());
             } else {
-                if (numberOfPixels + widthWord + spaceSize >= widthBitmap - 2 * epsilon) {
-                    numberOfPixels = 0;
+                if (widthLine >= widthBitmap - 2 * EPSILON) {
                     indexOfLine++;
                     StringBuilder stringBuilder = new StringBuilder();
                     stringBuilder.append(words[i]);
-                    builderArrayList.add(stringBuilder);
+                    stringArrayList.add(stringBuilder);
                 } else {
-                    numberOfPixels += (widthWord + spaceSize);
-                    builderArrayList.get(indexOfLine).append(" " + words[i]);
+                    stringArrayList.get(indexOfLine).append(" " + words[i]);
                 }
             }
         }
         List<String> list = new ArrayList<>();
 
-        for (StringBuilder s : builderArrayList) {
+        for (StringBuilder s : stringArrayList) {
             list.add(s.toString().replace('\n', ' '));
         }
 
-        int heightBitmap = (heightTemp) * list.size() + 2 * epsilon;
+        return list;
+    }
+
+
+    public Bitmap textToBitmap(int textSize, int widthBitmap, int colorIndex) {
+
+        List<String> list = textToList(textSize, widthBitmap);
+
+        Paint paint = new Paint();
+        paint.setTextSize(textSize);
+
+        Rect rect = new Rect();
+        paint.getTextBounds(text, 0, text.length(), rect);
+        int heightTemp = rect.height();
+
+        int heightBitmap = (heightTemp) * list.size() + 2 * EPSILON;
 
         int powerOfHeight = (int) (Math.log(heightBitmap) / Math.log(2)) + 1;
         heightBitmap = (int) Math.pow(2, powerOfHeight);
-
 
         Bitmap image = Bitmap.createBitmap(widthBitmap, heightBitmap, Bitmap.Config.ARGB_4444);
         Canvas canvas = new Canvas(image);
@@ -138,12 +111,11 @@ public class StringAdapter {
         canvas.drawRect(0, 0, widthBitmap, heightBitmap, paint);
 
         paint.setColor(Color.WHITE);
-        canvas.drawRect(epsilon, epsilon, widthBitmap - epsilon, heightBitmap - epsilon, paint);
-
+        canvas.drawRect(EPSILON, EPSILON, widthBitmap - EPSILON, heightBitmap - EPSILON, paint);
 
         paint.setColor(Color.BLACK);
         for (int i = 0; i < list.size(); i++) {
-            canvas.drawText(list.get(i), epsilon, epsilon + (i + 1) * heightTemp, paint);
+            canvas.drawText(list.get(i), EPSILON, EPSILON + (i + 1) * heightTemp, paint);
         }
 
         return image;
--- app/src/main/java/ch/epfl/sweng/project/engine3d/components/PanoramaInfoObject.java
+++ app/src/main/java/ch/epfl/sweng/project/engine3d/components/PanoramaInfoObject.java
@@ -11,17 +11,22 @@ public final class PanoramaInfoObject extends PanoramaObject {
     private static final String TEXTURE_TAG = "PanoramaInfoObject";
     private static final int ICON_INDEX = R.drawable.info_tex;
     private final String textInfo;
+    private final double theta;
+    private final double phi;
+
 
     public PanoramaInfoObject(double theta, double phi, String textInfo) {
         super(theta, phi, TEXTURE_TAG, ICON_INDEX);
         this.textInfo = textInfo;
         enableLookAt();
         setLookAt(new Vector3(0, 0, 0));
+        this.theta = theta;
+        this.phi = phi;
     }
 
 
     @Override
     public void reactWith(PanoramaRenderer p) {
-        p.displayText(textInfo);
+        p.displayText(textInfo, theta, phi);
     }
 }
