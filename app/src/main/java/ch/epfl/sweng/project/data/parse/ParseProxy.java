6bd84b3a8495c1d5fa5f6de8a6efb2925674be6d
QuentinBacuet <quentin.bacuet@epfl.ch>
2016-12-07 18:04:49 +0100

    Try to correct jenkins bug

--- app/src/main/java/ch/epfl/sweng/project/data/parse/ParseProxy.java
+++ app/src/main/java/ch/epfl/sweng/project/data/parse/ParseProxy.java
@@ -23,8 +23,8 @@ public enum ParseProxy {
     PROXY;
 
     private static final long INTERNET_TIMEOUT = 10000L;
-    private static final long QUERY_TIMEOUT = 3014L;
-    private static final long QUERY_2ND_TIMEOUT = 1014L;
+    private static final long QUERY_TIMEOUT = 5014L;
+    private static final long QUERY_2ND_TIMEOUT = 1514L;
     private static final String TAG = "ParseProxy";
 
     private long internetUnavailableTime = 0;
