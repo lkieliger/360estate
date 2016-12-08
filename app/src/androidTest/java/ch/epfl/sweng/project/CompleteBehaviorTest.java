59379d46909601cfcda3efdff1668fb7155216cc
Călin Iorgulescu <calin.iorgulescu@gmail.com>
2016-12-08 12:53:08 +0100

    Update CompleteBehaviorTest.java

--- app/src/androidTest/java/ch/epfl/sweng/project/CompleteBehaviorTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/project/CompleteBehaviorTest.java
@@ -14,6 +14,7 @@ import org.hamcrest.TypeSafeMatcher;
 import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.security.SecureRandom;
 
